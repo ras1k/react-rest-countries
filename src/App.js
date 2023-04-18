7d635449bbc4fa54d8f9b88e622edf31ae581588
ras1k <rasikabdullah@gmail.com>
2023-04-18 15:02:06 +0600

    extension pack addded

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
+// import { useState } from 'react';
+// import { useEffect } from 'react';
+import Countries from './components/Countries/Countries';
+import Header from './components/Header/Header';
+import Person from './components/Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <LoadContries></LoadContries>
+      <Header></Header>
+      <Countries></Countries>
+      <Person></Person>
     </div>
   );
 }
 
-function LoadContries (){
-  const [countries, setCountries] = useState([]);
-  useEffect(()=>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => setCountries(data))
-  }, [])
-  return (
-    <div>
-      <h1>Visiting Every Countries In The World</h1>
-      <h3>Available Countries : {countries.length}</h3>
-      {
-        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
-      }
-    </div>
-  )
-}
+// function LoadContries (){
+//   const [countries, setCountries] = useState([]);
+//   useEffect(()=>{
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res => res.json())
+//     .then(data => setCountries(data))
+//   }, [])
+//   return (
+//     <div>
+//       <h1>Visiting Every Countries In The World</h1>
+//       <h3>Available Countries : {countries.length}</h3>
+//       {
+//         countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+//       }
+//     </div>
+//   )
+// }
 
-function Country(props){
-  return(
-    <div>
-      <h2>Name : {props.name}</h2>
-      <h4>Population : {props.population}</h4>
-    </div>
-  )
-}
+// function Country(props){
+//   return(
+//     <div>
+//       <h2>Name : {props.name}</h2>
+//       <h4>Population : {props.population}</h4>
+//     </div>
+//   )
+// }
 
 export default App;
